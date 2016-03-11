409363b8f11107f21c7b9bc41f54fcdf5571321a
Jesper Skovgaard Nielsen <nulpunkt@gmail.com>
2016-03-11 13:22:05 +0100

    Better naming

--- src/Nulpunkt/Yesql/Repository.php
+++ src/Nulpunkt/Yesql/Repository.php
@@ -6,7 +6,7 @@ class Repository
 {
     private $db;
     private $sqlFile;
-    private $methods;
+    private $statements;
 
     public function __construct(\PDO $db, $sqlFile)
     {
@@ -17,16 +17,16 @@ class Repository
     public function __call($name, $args)
     {
         $this->load();
-        return $this->methods[$name]->execute($this->db, $args);
+        return $this->statements[$name]->execute($this->db, $args);
     }
 
     private function load()
     {
-        if ($this->methods) {
+        if ($this->statements) {
             return;
         }
 
-        $this->methods = [];
+        $this->statements = [];
 
         $currentMethod = null;
         foreach (file($this->sqlFile) as $line) {
@@ -35,10 +35,10 @@ class Repository
                 preg_match("/\bname:\s*([a-zA-Z_0-9]+)/", $line, $m);
                 $currentMethod = $m[1];
             } elseif ($currentMethod && stripos($line, 'select') === 0) {
-                $this->methods[$currentMethod] = new Statement\Select($line);
+                $this->statements[$currentMethod] = new Statement\Select($line);
                 $currentMethod = null;
             } elseif ($currentMethod && stripos($line, 'insert') === 0) {
-                $this->methods[$currentMethod] = new Statement\Insert($line);
+                $this->statements[$currentMethod] = new Statement\Insert($line);
                 $currentMethod = null;
             }
         }
