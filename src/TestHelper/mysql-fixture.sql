b7a926fadd6d31635bbe07154fabc57d3b634a8d
Jesper Skovgaard Nielsen <nulpunkt@gmail.com>
2022-05-17 10:18:55 +0200

    Select a database

--- src/TestHelper/mysql-fixture.sql
+++ src/TestHelper/mysql-fixture.sql
@@ -1,8 +1,8 @@
 CREATE DATABASE `yesql`;
 CREATE USER 'yesql'@'localhost' IDENTIFIED BY 'yesql';
 GRANT ALL PRIVILEGES ON yesql.* TO 'yesql'@'%' WITH GRANT OPTION;
-DROP TABLE IF EXISTS `test_table`;
-CREATE TABLE `test_table` (
+DROP TABLE IF EXISTS `yesql`.`test_table`;
+CREATE TABLE `yesql`.`test_table` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `something` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
